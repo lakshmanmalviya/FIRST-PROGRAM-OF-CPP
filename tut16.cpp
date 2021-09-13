1a703a12c26afb3604b8c8dc232555d191e85542
lakshmanmalviya <90620208+lakshmanmalviya@users.noreply.github.com>
2021-09-13 18:09:32 +0530

    Add files via upload

---
+++ tut16.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int a = 30;
+    int b = 55;
+   
+    cout << round(a);
+
+    // cout<<max(a,b);y
+
+    // cout << log(a, b);
+    // cout << tan(x, y);
+
+    // setw;
+    // int a = 3, b = 57, c = 123;
+    // cout << " These value are without setw " << endl;
+    // cout << " The  value of a without setw is :" << a << endl;
+    // cout << " The  value of  b without setw is :" << b << endl;
+    // cout << " The  value of  c without setw is :" << c << endl;
+
+    // cout << " The value of a with setw is :" << setw(4) << a << endl;
+    // cout << " The value of  b with setw is :" << setw(4) << b << endl;
+    // cout << " The value of  c with setw is :" << setw(4) << c << endl;
+
+    return 0;
+}
